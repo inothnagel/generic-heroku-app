267171a45d31d7ae257139b9a062461f6faf6976
Ignatius Nothnagel <inothnagel@gmail.com>
2012-11-01 18:06:19 +0200

    added daily email code

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,8 +1,8 @@
 class AdminMailer < ActionMailer::Base
-  default from: 'SET ADMIN EMAIL HERE'
+  default from: 'SET_FROM_EMAIL_HERE'
 
   def new_registration_email(user)
-    admin_email = 'SET ADMIN EMAIL HERE'
+    admin_email = 'SET_ADMIN_EMAIL_HERE'
 
     @user = user
     mail(:to => admin_email, :subject => "New Registration: #{user.email}")
